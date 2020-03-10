02cf31ac6de92e8690bd32d8ff4ff26d75272026
jonathandelaney <jonathandelaney@canvas-inc.com>
2020-03-10 16:09:54 -0500

    new branch test for file conversion

---
+++ WpfApp2/MainWindow/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using WpfApp2.LoanEstimate;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var form = new LoanEstimateInputWindow();
+            form.Show();
+            this.Close();
+        }
+    }
+}
